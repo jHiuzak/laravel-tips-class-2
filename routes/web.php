3a42c6483f0bdb31bd6d854821e56778ce5acd47
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2022-10-20 10:10:32 -0300

    Put Route::group as comment for tests

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,29 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+// Route::group(['namespace' => 'App\Http\Controllers'], function()
+// {
+    Route::get('/users', ['UserController@index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+
+    Route::put('/users/update/{user}', [UserController::class, 'update'])->name('users.update');
+
+    Route::delete('/users/destroy/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+// });
+
+// Route::get('/users', [UserController::class, 'index'])->name('users.index');
+// Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+// Route::get('/users/edit/{user}', [UserController::class, 'edit'])->name('users.edit');
+// Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+
+// Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
 
 // patch and put do the same things, but there are conventions
-Route::put('/users/update/{user}', [UserController::class, 'update'])->name('users.update');
+// Route::put('/users/update/{user}', [UserController::class, 'update'])->name('users.update');
 
-Route::delete('/users/destroy/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+// Route::delete('/users/destroy/{user}', [UserController::class, 'destroy'])->name('users.destroy');
